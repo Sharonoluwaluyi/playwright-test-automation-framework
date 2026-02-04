c1c0c5bce68a8448bd54f28a83985d39dd3f4887
Sharonoluwaluyi <96075632+Sharonoluwaluyi@users.noreply.github.com>
2026-02-04 15:01:25 +0000

    Update LoginPage.ts

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -1,15 +1,23 @@
 import { Page } from '@playwright/test';
 
 export class LoginPage {
-  constructor(private page: Page) {}
+  private page: Page;
+
+  constructor(page: Page) {
+    this.page = page;
+  }
 
   async navigate() {
-    await this.page.goto('https://example.com/login');
+    await this.page.goto('https://the-internet.herokuapp.com/login');
   }
 
   async login(username: string, password: string) {
     await this.page.fill('#username', username);
     await this.page.fill('#password', password);
-    await this.page.click('#login');
+    await this.page.click('button[type="submit"]');
+  }
+
+  async getSuccessMessage() {
+    return this.page.locator('#flash');
   }
 }
